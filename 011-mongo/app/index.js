c5d6c9e321b81717267993fd3f09e726c9489d02
Leonid <moryakovl@gmail.com>
2021-10-10 13:15:00 +0300

    MongoDB Atlas [011-momngo]

--- 011-mongo/app/index.js
+++ 011-mongo/app/index.js
@@ -12,9 +12,14 @@ const booksRouter = require('./routes/books');
 const { buildUrl } = require('./utils');
 
 const PORT = process.env.PORT || 3000;
-const UserDB = process.env.DB_USERNAME || 'witcher';
-const PasswordDB = process.env.DB_PASSWORD || '12345';
+const UserDB = process.env.DB_USERNAME;
+const PasswordDB = process.env.DB_PASSWORD;
 const NameDB = process.env.DB_NAME || 'app_database'
+
+const UserAtlasDB = process.env.DB_ATLAS_USERNAME;
+const PasswordAtlasDB = process.env.DB_ATLAS_PASSWORD;
+const NameAtlasDB = process.env.DB_ATLAS_NAME || 'app_database';
+
 const HostDb = process.env.DB_HOST || 'mongodb://mongodb/'
 const APP_URL = '/api';
 const SERVICES_URLS = {
@@ -47,18 +52,17 @@ app.use(errorMiddleware);
 
 async function start() {
     try {
-        //const UrlDB = `mongodb+srv://${UserDB}:${PasswordDB}@cluster0.grfrs.mongodb.net/${NameDB}`;
-        //const UrlDB = `mongodb://localhost:27017/mydb`;
-        //const UrlDB = `mongodb://${UserDB}:${PasswordDB}@localhost:27017/mydb`;
-        //await mongoose.connect(UrlDb);
+        console.log(`mongodb+srv://${UserAtlasDB}:${PasswordAtlasDB}@cluster0.ctq8h.mongodb.net/${NameAtlasDB}?retryWrites=true&w=majority`);
+        const UrlDB = `mongodb+srv://${UserAtlasDB}:${PasswordAtlasDB}@cluster0.ctq8h.mongodb.net/${NameAtlasDB}?retryWrites=true&w=majority`;
+        await mongoose.connect(UrlDB);
         
-        await mongoose.connect(HostDb, {
-            user: UserDB,
-            pass: PasswordDB,
-            dbName: NameDB,
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        // await mongoose.connect(HostDb, {
+        //     user: UserDB,
+        //     pass: PasswordDB,
+        //     dbName: NameDB,
+        //     useNewUrlParser: true,
+        //     useUnifiedTopology: true
+        // });
 
         app.listen(PORT, '0.0.0.0', () => {
             console.log(`Express app is listening at http://localhost:${PORT}`)
